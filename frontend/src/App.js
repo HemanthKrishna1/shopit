a32353599feb89fd68ee9baba2c44087edebd094
Hemanth Krishna <k.hemanth1999@gmail.com>
2023-10-22 20:05:54 -0500

    Redux toolkit Implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import Header from "./components/layout/Header";
 import "./App.css";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import ProductDetails from "./components/product/productDetails";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Toaster position="top-center" />
         <Header />
 
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
           </Routes>
         </div>
 
