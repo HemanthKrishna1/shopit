56190630a41591e7105d38f225da7c17fbf418f0
Hemanth Krishna <k.hemanth1999@gmail.com>
2023-10-25 17:49:05 -0500

    Added Filters

--- frontend/src/redux/api/productsApi.js
+++ frontend/src/redux/api/productsApi.js
@@ -5,7 +5,17 @@ export const productApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "/api/v1" }),
   endpoints: (builder) => ({
     getProducts: builder.query({
-      query: (params) => "/products",
+      query: (params) => ({
+        url: "/products",
+        params: {
+          page: params?.page,
+          keyword: params?.keyword,
+          "price[gte]": params.min,
+          "price[lte]": params.max,
+          category: params.category,
+          "ratings[gte]": params?.ratings,
+        },
+      }),
     }),
     getProductDetails: builder.query({
       query: (id) => `/products/${id}`,
